23f2e87419eba4c455320ede83de9e0d2aec3275
Hassan <21100120@lums.edu.pk>
2020-04-30 06:18:32 +0500

    Added parser

--- lums-eat/server.js
+++ lums-eat/server.js
@@ -9,6 +9,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(
+    bodyParser.urlencoded({
+      extended: false
+    })
+  );
+
+app.use(bodyParser.json());
+
+//Connecting to MongoDB
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
@@ -20,11 +29,12 @@ connection.once('open', () => {
     console.log("MongoDB database connection established!")
 })
 
+//Defining routes for our MongoDB documents
+
 const usersRoute = require('./routes/users');
 const restaurantRoute = require('./routes/restaurants');
 const ordersRoute = require('./routes/orders');
 const menuRoute = require('./routes/menuitems');
-
 
 app.use('/restaurants', restaurantRoute);
 app.use('/users', usersRoute);

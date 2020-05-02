12cc2fa8bef49180ba1e6dd53a5ff4f0bf908f4f
haseebch10 <haseebch10@gmail.com>
2020-05-02 19:20:42 +0500

    fixed css

--- lums-eat/client/src/components/collection-preview/collection-preview.jsx
+++ lums-eat/client/src/components/collection-preview/collection-preview.jsx
@@ -11,7 +11,7 @@ const CollectionPreview = ({ items }) => {
   return(
   <div className='collection-preview'>
     <h1 className='title'> {items[0].category} </h1>
-    <div className='preview'>
+    <div className='items'>
       {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}

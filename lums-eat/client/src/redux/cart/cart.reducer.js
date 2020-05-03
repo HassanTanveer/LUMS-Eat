f9b773e27c4bdfeaf2285429f2ef4cad592280ae
haseebch10 <haseebch10@gmail.com>
2020-05-03 23:27:52 +0500

    cart

--- lums-eat/client/src/redux/cart/cart.reducer.js
+++ lums-eat/client/src/redux/cart/cart.reducer.js
@@ -27,7 +27,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: state.cartItems.filter(
-          cartItem => cartItem.id !== action.payload.id
+          cartItem => cartItem.ItemID !== action.payload.ItemID
         )
       };
     default:

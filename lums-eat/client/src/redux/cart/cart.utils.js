f7d11b5c89e7d46686b221e55174b28478eda1cf
Hassan <21100120@lums.edu.pk>
2020-05-04 20:41:18 +0500

    Fixed cart

--- lums-eat/client/src/redux/cart/cart.utils.js
+++ lums-eat/client/src/redux/cart/cart.utils.js
@@ -1,55 +1,51 @@
-export const addItemToCart = (cartItems, cartItemToAdd) => {
-  const existingCartItem = cartItems.find(
-    cartItem => cartItem.ItemID === cartItemToAdd.ItemID
-  );
+import isEmpty from "is-empty";
 
-  var z
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+
+  
+  if(isEmpty(cartItems)){
+    return [...cartItems, { ...cartItemToAdd, quantity: 1}];
+  }
+  else{  
+    
+    const existingCartItem = cartItems.find(
+    cartItem => cartItem.ItemID === cartItemToAdd.ItemID
+    );
+
+  let z
 
   cartItems.map(cartItem =>
-    cartItem.RestaurantID  ? ( z=2 )
-      : z=1
+    cartItem.RestaurantID  ? (z = 2)
+      : (z = 1)
   );
 
   
-    if(z===1){
-      return [...cartItems, { ...cartItemToAdd, quantity: 1}];
+  if(z===1){
+    return [...cartItems, { ...cartItemToAdd, quantity: 1}];
+  }
     
-    }
-    
-    
-  
- 
-
   if (existingCartItem) {
     return cartItems.map(cartItem =>
       cartItem.ItemID === cartItemToAdd.ItemID
-        ? { ...cartItem, quantity: cartItem.quantity + 1 }
-        : cartItem
+        ? ({ ...cartItem, quantity: cartItem.quantity + 1 })
+        : (cartItem)
     );
   }
 
-  var a
+  let a
 
   cartItems.map(cartItem =>
     cartItem.RestaurantID === cartItemToAdd.RestaurantID
       ? ( a=2 )
-      : a=1
+      : (a = 1)
   );
-
  
-
-
-  
   if(a===2){
-  
-  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
-    }
-
-   
-
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  }
   else{
     return [...cartItems];
-  }
+  }}
 };
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
